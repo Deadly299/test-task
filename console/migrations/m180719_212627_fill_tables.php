e5880968559b03197b946ffbf4a629cba2c1834c
Павел Арченков <deadly299@gmail.com>
2018-07-21 00:50:18 +0500

    Change README and migration

--- console/migrations/m180719_212627_fill_tables.php
+++ console/migrations/m180719_212627_fill_tables.php
@@ -19,24 +19,21 @@ class m180719_212627_fill_tables extends Migration
         $this->insert('table2', ['name' => 'second item table2 table1', 'table1_id' => 2]);
 
 
-        $this->insert('table3', [
-            'table2_id' => 1,
-            'field1' => 'text field 1.1',
-            'field2' => 'text field 2.1',
-            'field3' => 'text field 3.1',
-        ]);
-        $this->insert('table3', [
-            'table2_id' => 1,
-            'field1' => 'text field 1.2',
-            'field2' => 'text field 2.2',
-            'field3' => 'text field 3.2',
-        ]);
-        $this->insert('table3', [
-            'table2_id' => 2,
-            'field1' => 'text field 1.2',
-            'field2' => 'text field 2.2',
-            'field3' => 'text field 3.2',
-        ]);
+        $this->insert('table3', ['table2_id' => 1, 'field1' => 'text field 1.1', 'field2' => 'text field 2.1', 'field3' => 'text field 3.1',]);
+        $this->insert('table3', ['table2_id' => 1, 'field1' => 'text field 1.1', 'field2' => 'text field 2.1', 'field3' => 'text field 3.1',]);
+        $this->insert('table3', ['table2_id' => 1, 'field1' => 'text field 1.1', 'field2' => 'text field 2.1', 'field3' => 'text field 3.1',]);
+        $this->insert('table3', ['table2_id' => 1, 'field1' => 'text field 1.1', 'field2' => 'text field 2.1', 'field3' => 'text field 3.1',]);
+        $this->insert('table3', ['table2_id' => 1, 'field1' => 'text field 1.1', 'field2' => 'text field 2.1', 'field3' => 'text field 3.1',]);
+        $this->insert('table3', ['table2_id' => 1, 'field1' => 'text field 1.1', 'field2' => 'text field 2.1', 'field3' => 'text field 3.1',]);
+        $this->insert('table3', ['table2_id' => 1, 'field1' => 'text field 1.1', 'field2' => 'text field 2.1', 'field3' => 'text field 3.1',]);
+        $this->insert('table3', ['table2_id' => 1, 'field1' => 'text field 1.1', 'field2' => 'text field 2.1', 'field3' => 'text field 3.1',]);
+        $this->insert('table3', ['table2_id' => 1, 'field1' => 'text field 1.1', 'field2' => 'text field 2.1', 'field3' => 'text field 3.1',]);
+        $this->insert('table3', ['table2_id' => 1, 'field1' => 'text field 1.1', 'field2' => 'text field 2.1', 'field3' => 'text field 3.1',]);
+        $this->insert('table3', ['table2_id' => 1, 'field1' => 'text field 1.1', 'field2' => 'text field 2.1', 'field3' => 'text field 3.1',]);
+        $this->insert('table3', ['table2_id' => 1, 'field1' => 'text field 1.1', 'field2' => 'text field 2.1', 'field3' => 'text field 3.1',]);
+
+        $this->insert('table3', ['table2_id' => 2, 'field1' => 'text field 1.1', 'field2' => 'text field 2.1', 'field3' => 'text field 3.1',]);
+
     }
 
     public function down()
